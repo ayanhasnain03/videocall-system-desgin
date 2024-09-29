eb802acbbc6e1539b2b8453c6c26cb43f4951e0d
Ayan <ayan2572006@gmail.com>
2024-09-29 19:09:04 +0530

    reciver set sender set

--- frontend/src/components/Receiver.tsx
+++ frontend/src/components/Receiver.tsx
@@ -6,6 +6,18 @@ const Receiver = () => {
     socket.onopen = () => {
       socket.send(JSON.stringify({ type: "receiver" }));
     };
+    socket.onmessage = async (event) => {
+      const message = JSON.parse(event.data);
+      if (message.type === "createOffer") {
+        const pc = new RTCPeerConnection();
+        pc.setRemoteDescription(message.sdp);
+        const answer = await pc.createAnswer();
+        pc.setLocalDescription(answer);
+        socket.send(
+          JSON.stringify({ type: "createAnswer", sdp: pc.localDescription })
+        );
+      }
+    };
   }, []);
 
   return <div>Receiver</div>;
--- frontend/src/components/Sender.tsx
+++ frontend/src/components/Sender.tsx
@@ -2,14 +2,27 @@ import { useEffect, useState } from "react";
 
 const Sender = () => {
   const [socket, setsocket] = useState<WebSocket | null>(null);
+
   useEffect(() => {
     const socket = new WebSocket("ws://localhost:8080");
     socket.onopen = () => {
       socket.send(JSON.stringify({ type: "sender" }));
     };
   }, []);
-  function startSendingVideo() {
+
+  async function startSendingVideo() {
+    if (!socket) return;
     //create an offer
+    const pc = new RTCPeerConnection();
+    const offer = await pc.createOffer(); //sdp
+    pc.setLocalDescription(offer);
+    socket?.send(JSON.stringify({ type: "createOffer", sdp: offer.sdp }));
+    socket.onmessage = async (event) => {
+      const data = JSON.parse(event.data);
+      if (data.type === "createAnswer") {
+        pc.setRemoteDescription(data.sdp);
+      }
+    };
   }
   return (
     <div>
